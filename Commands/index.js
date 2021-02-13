87817f7ba59b767e6e2806f548c20d0981b5a6ba
Daniel Tran <danieltran219@gmail.com>
2021-02-13 07:07:27 -0800

    connecting to mongo database

--- Commands/index.js
+++ Commands/index.js
@@ -4,16 +4,46 @@ const fetch = require('node-fetch')
 const bot = new Discord.Client()
 
 //logs in the bot for testing
-//const config = require('./config.json')
-const firstMessage = require('./first-message')
+const config = require('./config.json')
 const command = require('./command')
+const firstMessage = require('./first-message')
+const privateMessage = require('./private-message')
+const mongo = require('./mongo')
+const welcome = require('./welcome')
 
-bot.on('ready', () => {
+bot.on('ready', async () => {
   console.log('Hello World')
+
+  //connects to mongo servers when bot goes online
+  await mongo().then((mongoose) => {
+    try {
+      //try some code
+      console.log('Connected to mongo')
+    } finally {
+      //will always run
+      mongoose.connection.close()
+      //this will close the database
+    }
+  })
+
+  //welcome bot per server rules using mongo servers
+  welcome(bot)
 
   //sends or edits the bots message
   // firstMessage(bot, '807843037425696781', 'hello world', ['❤'])
 
+  //dm's the user the current usable commands
+  privateMessage(bot, '!help', `Here are the current commands:
+ !ping = test if bot is online
+ !servers = display amount of members in the current server 
+ !cc = clear all messages in a channel (ADMIN)
+ !status <input-phrase> = change the status of the bot
+ !rquote = display a random quote
+ !dquote = display the quote of the day
+ !joke = display a random joke 
+  `)
+
+  //checks to see if bot is online
   command(bot, ['ping', 'test'], (message) => {
     message.channel.send('pong!')
   })
@@ -35,6 +65,7 @@ bot.on('ready', () => {
     }
   })
 
+  //changes the bots status
   command(bot, 'status', message => {
     const content = message.content.replace('!status ', '')
     //"!status hello world" -> "hello world"
@@ -54,13 +85,13 @@ bot.on('ready', () => {
   })
 
   //checks for daily or random quote using an api
-  command(bot, 'rquote', async(message) => {
+  command(bot, 'rquote', async (message) => {
     let getQuote = async () => {
-        let result = await fetch
-          ('https://zenquotes.io/api/random/')
-        let json = await result.json()
-        return json
-      }
+      let result = await fetch
+        ('https://zenquotes.io/api/random/')
+      let json = await result.json()
+      return json
+    }
 
     let quote = await getQuote()
     message.reply(`
@@ -69,13 +100,13 @@ bot.on('ready', () => {
   })
 
   //checks for daily or random quote using an api
-  command(bot, 'dquote', async(message) => {
+  command(bot, 'dquote', async (message) => {
     let getQuote = async () => {
-        let result = await fetch
-          ('https://zenquotes.io/api/today/')
-        let json = await result.json()
-        return json
-      }
+      let result = await fetch
+        ('https://zenquotes.io/api/today/')
+      let json = await result.json()
+      return json
+    }
 
     let quote = await getQuote()
     message.reply(`
@@ -84,7 +115,7 @@ bot.on('ready', () => {
   })
 
   //generates a random joke from an api
-  command(bot, 'joke', async(message) => {
+  command(bot, 'joke', async (message) => {
     let getJoke = async () => {
       let result = await fetch
         ('https://official-joke-api.appspot.com/random_joke')
@@ -110,9 +141,8 @@ bot.on('ready', () => {
     message.channel.bulkDelete(num);
     message.channel.send(`Deleted ${num} Total`);
   })
-
 })
 
-//bot.login(config.token);
+bot.login(config.token);
 //for heroku
-bot.login(process.env.BOT_TOKEN)
+//bot.login(process.env.BOT_TOKEN)
---
+++ Commands/welcome.js
@@ -0,0 +1,93 @@
+const mongo = require('./mongo')
+const command = require('./command')
+const welcomeSchema = require('./schemas/welcome-schema')
+
+module.exports = (bot) => {
+    //!setwelcome <message>
+    const cache = {} //guildId: [channelID, text]
+
+    command(bot, 'setwelcome', async (message) => {
+        const { member, channel, content, guild } = message
+
+        if (!member.hasPermission('ADMINISTRATOR')) {
+            channel.send('You do not have permission to run this command')
+            return
+        }
+
+        let text = content
+
+        const split = text.split(' ')
+        if (split.length < 2) {
+            channel.send('Please provide a welcome message')
+            return
+        }
+
+        split.shift()
+        text = split.join(' ')
+
+        //stores in local cache so it does not have to fetch from data base
+        cache[guild.id] = [channel.id, text]
+
+        await mongo().then(async (mongoose) => {
+            try {
+                //if the data is already there, find it and update
+                await welcomeSchema.findOneAndUpdate({
+                    _id: guild.id
+                }, {
+                    _id: guild.id,
+                    channelId: channel.id,
+                    text,
+                }, {
+                    //insert and to update
+                    upsert: true
+                })
+            } finally {
+                mongoose.connection.close()
+            }
+        })
+    })
+    const onJoin = async member => {
+        const { guild } = member
+
+        let data = cache[guild.id]
+
+        if (!data) {
+            //if there is no local data, must fetch from mongo database
+            console.log('FETCHING FROM DATABASE')
+            await mongo().then(async (mongoose) => {
+                try {
+                    const result = await welcomeSchema.findOne({ _id: guild.id })
+
+                    cache[guild.id] = data = [result.channelId, result.text]
+                } finally {
+                    mongoose.connection.close()
+                }
+            })
+        }
+        const channelId = data[0]
+        const text = data[1]
+
+        const channel = guild.channels.cache.get(channelId)
+        //this tags the user who joins the server
+        channel.send(text.replace(/<@>/g, `<@${member.id}>`))
+    }
+
+    //to simulate someone joining the server for testing purposes
+    command(bot, 'simjoin', message => {
+        onJoin(message.member)
+    })
+
+    //when a user joins the server
+    bot.on('guildMemberAdd', member => {
+        onJoin(member)
+    })
+}
+
+
+
+
+
+
+
+
+
