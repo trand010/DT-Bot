9900c086865f38624e9a2bc93617e74592705ec8
Daniel Tran <danieltran219@gmail.com>
2021-03-31 04:46:02 -0700

    updated ping command

--- commands/misc/ping.js
+++ commands/misc/ping.js
@@ -2,8 +2,12 @@ module.exports = {
     commands: ['ping'],
     minArgs: 0,
     maxArgs: 0,
-    callback: (message, arguments, text) => {
-        message.reply('Pong!')
+    callback: (message, arguments, text, bot) => {
+        message.reply('Calculating ping...').then(resultMessage => {
+            const ping = resultMessage.createdTimestamp - message.createdTimestamp
+
+            resultMessage.edit(`Bot Latency: ${ping}, API Latency: ${bot.ws.ping}`)
+        })
     },
 }
 
